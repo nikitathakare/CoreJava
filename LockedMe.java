284a17ee2b123b1cb60a0a8443b970aedcaa0792
Nikita <nikita007thakare@gmail.com>
2022-02-24 23:49:04 +0530

    LockedMe is added

---
+++ LockedMe.java
@@ -0,0 +1,105 @@
+package Files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class LockedMe{
+	static String path1 = "C:\\Users\\n.suresh.thakare\\Assingment\\FileHandeling\\m";
+	static int key ;
+	static int key1;
+	
+	
+	public static void main(String[] args)   {
+		FileOption obj =new FileOption();
+		displayMenu();
+		
+		
+		do {
+			options();
+			Scanner sc = new Scanner(System.in);
+			try {
+				key = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println(e+"**********Please Enter correct option***************");
+				sc.next();
+			}
+
+			switch (key) {
+			case 1:
+				System.out.println("=============================");
+				System.out.println(" Files Present in Directory ::");
+				System.out.println("=============================");
+				obj.allFiles(path1);
+				;
+				System.out.println(
+						"*****************************************************************************************");
+				break;
+			case 2:
+				obj.fileOptions();
+				Scanner sc1 = new Scanner(System.in);
+				try {
+					key1 = sc.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println(e + "Please Enter correct file option");
+					sc.next();
+				}
+				obj.switchCase(key1);
+				break;
+			case 3:
+				System.out.println(
+						"=======================================================================================");
+				System.out.println("\t\t\t Thanks for using LockedMe.com Application ");
+				System.out.println(
+						"=======================================================================================");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("****** Incorrect Input **********");
+				options();
+				try {
+					key = sc.nextInt();
+				} catch (InputMismatchException e) {
+					System.out.println("Please Enter Incorrect option");
+					sc.next();
+				}
+				break;
+			}
+
+		}
+
+		while (key > 0);
+
+	}
+
+	public static void displayMenu() {
+
+		System.out.println("\n=======================================================================================");
+		System.out.println("\t\t\tThis is Lock Me Application");
+		System.out.println("\t\t\t***Author:Nikita Thakare***");
+		System.out.println("=======================================================================================");
+
+	}
+
+	public static void options() {
+
+		System.out.println("\n=======================================================================================");
+		System.out.println("\t\t Features of Lock Me Application :");
+		System.out.println("=======================================================================================");
+		System.out.println("1. Display All Files");
+		System.out.println("2. Show File Options");
+		System.out.println("3. Exit Menu");
+		System.out.println("\nPlease select option : ");
+
+	}
+
+	
+	
+	
+
+}
